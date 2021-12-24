af0b3b40a195a82ca951e7903675d3d35a863c59
Daniel <dcloran@ucdavis.edu>
2021-12-24 11:58:32 -0800

    initial commit

---
+++ node_classification.py
@@ -0,0 +1,11 @@
+import dgl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import dgl.data
+
+dataset = dgl.data.CoraGraphDataset()
+print('Number of categories:', dataset.num_classes)
+
+g = dataset[0]
